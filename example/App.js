cfb012f36d97b4bb9a27b3d4b0c65f8cc5961569
Oleg Akbarov <olegakbarov@gmail.com>
2016-09-26 21:39:37 +0300

    fixed React presence

--- example/App.js
+++ example/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Component from 'Component';
+import Component from 'Component'; //eslint-disable-line
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
         <Component />
-      </div>
     );
   }
 }
--- example/root.js
+++ example/root.js
@@ -1,6 +1,8 @@
 /* eslint-env browser */
 /* eslint no-unused-vars: 0 */
+
 import DOM from 'react-dom';
+import React from 'react';
 import App from './App';
 
 DOM.render(
