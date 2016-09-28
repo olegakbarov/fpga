53d1f64fa547b73d887f75a1fe0272f58935a92b
Amila Welihinda <amilajack@gmail.com>
2016-09-28 13:32:46 -0700

    Updated deps, fixed linter configuration and errors

--- example/App.js
+++ example/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Component from 'Component';
 
 export default class App extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* @flow */
 import React from 'react';
 
 export default class Component extends React.Component {
